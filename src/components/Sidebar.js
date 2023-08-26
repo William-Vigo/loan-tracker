155d339ad24f3e2effff9fddb3851775c5c9c3ea
William-Vigo <d3rboyfresh123@hotmail.com>
2023-08-26 03:35:10 -0400

    sidebar css pt1

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import { Link, Route, Routes} from 'react-router-dom';
 import App from '../App';
-import Test from './test';
-
+import homeIcon from '../assets/home.png';
+import clientsIcon from '../assets/clients.png';
+import paymentsIcon from '../assets/clients.png';
+import transaccionesIcon from '../assets/receipts.png';
+import './Sidebar.css';;
 
 function Sidebar() {
     return (
-        <>
+        <div className="sidebar">
             <div>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                </ul>
-                <ul>
-                    <li><Link to="/Clientes">Clientes</Link></li>
-                </ul>
-                <ul>
-                    <li><Link to="/Pagos">Pagos</Link></li>
-                </ul>
-                <ul>
-                    <li><Link to="/Transacciones">Transacciones</Link></li>
+                    <li><Link to="/"><img className="sidebar-icon" src={homeIcon}/>Home</Link></li>
+                    <li><Link to="/Clientes"><img className="sidebar-icon" src={clientsIcon}/>Clientes</Link>
+                        <ul id="sidebar-sublist">
+                            <li><Link to="/Clientes/NeuvoCliente">Nuevo Cliente</Link></li>
+                            <li><Link to="/Clientes/Lista">Lista de Clientes</Link></li>
+                        </ul>
+                    </li>
+                    <li><Link to="/Pagos"><img className="sidebar-icon" src={paymentsIcon}/>Pagos</Link></li>
+                    <li><Link to="/Transacciones"><img className="sidebar-icon" src={transaccionesIcon}/>Transacciones</Link></li>
                 </ul>
             </div>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/Clientes" element={<App />} />
+                <Route path="/Clientes/NeuvoCliente" element={<App />} />
+                <Route path="/Clientes/Lista" element={<App />} />
                 <Route path="/Pagos" element={<App />} />
                 <Route path="/Transacciones" element={<App />} />
             </Routes>
-        </>
+        </div>
     );
 }
 export default Sidebar
