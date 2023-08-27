8ecf155e2f5e939cddcfea05070de02ac81d80ec
William-Vigo <d3rboyfresh123@hotmail.com>
2023-08-27 02:05:04 -0400

    updated css to be more sleek

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link, Route, Routes} from 'react-router-dom';
 import App from '../App';
-import homeIcon from '../assets/home.png';
-import clientsIcon from '../assets/clients.png';
-import paymentsIcon from '../assets/clients.png';
-import transaccionesIcon from '../assets/receipts.png';
+import {ReactComponent as HomeIcon} from '../assets/home.svg';
+import {ReactComponent as ClientsIcon} from '../assets/clients.svg';
+import {ReactComponent as PaymentsIcon} from '../assets/clients.svg';
+import {ReactComponent as TransaccionesIcon} from '../assets/receipts.svg';
 import './Sidebar.css';;
 
 function Sidebar() {
@@ -12,15 +12,16 @@ function Sidebar() {
         <div className="sidebar">
             <div>
                 <ul>
-                    <li><Link to="/"><img className="sidebar-icon" src={homeIcon}/>Home</Link></li>
-                    <li><Link to="/Clientes"><img className="sidebar-icon" src={clientsIcon}/>Clientes</Link>
+                    <li className="group-title"><h5>Overview</h5></li>
+                    <li className="sidebar-link"><Link to="/"><HomeIcon className="sidebar-icon"/><span>Home</span></Link></li>
+                    <li className="sidebar-link"><Link to="/Clientes"><ClientsIcon className="sidebar-icon"/><span>Clientes</span></Link>
                         <ul id="sidebar-sublist">
-                            <li><Link to="/Clientes/NeuvoCliente">Nuevo Cliente</Link></li>
-                            <li><Link to="/Clientes/Lista">Lista de Clientes</Link></li>
+                            <li className="sidebar-sublist-link"><Link to="/Clientes/NeuvoCliente"><span>Nuevo Cliente</span></Link></li>
+                            <li className="sidebar-sublist-link"><Link to="/Clientes/Lista"><span>Lista de Clientes</span></Link></li>
                         </ul>
                     </li>
-                    <li><Link to="/Pagos"><img className="sidebar-icon" src={paymentsIcon}/>Pagos</Link></li>
-                    <li><Link to="/Transacciones"><img className="sidebar-icon" src={transaccionesIcon}/>Transacciones</Link></li>
+                    <li className="sidebar-link"><Link to="/Pagos"><PaymentsIcon className="sidebar-icon"/><span>Pagos</span></Link></li>
+                    <li className="sidebar-link"><Link to="/Transacciones"><TransaccionesIcon className="sidebar-icon"/><span>Transacciones</span></Link></li>
                 </ul>
             </div>
             <Routes>
