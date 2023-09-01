c8a3523b1f5ed6a7c855d6f8862d528606f74f8f
William-Vigo <d3rboyfresh123@hotmail.com>
2023-09-01 01:44:35 -0400

    refactor sidebar to use material ui

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Collapse, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { Box, Collapse, Drawer, List, ListItemButton, ListItemIcon, ListItemText, createTheme } from '@mui/material'
 import {ReactComponent as HomeIcon} from '../assets/home.svg';
 import {ReactComponent as ClientsIcon} from '../assets/clients.svg';
 import {ReactComponent as PaymentsIcon} from '../assets/clients.svg';
 import {ReactComponent as TransaccionesIcon} from '../assets/receipts.svg';
-import './Sidebar.css';import { color, margin } from '@mui/system';
-;
+import './Sidebar.css';
+import { ThemeProvider} from '@mui/system';
 
 const sidebarProps = [
     {
@@ -41,46 +41,84 @@ const sidebarProps = [
     },
 ]
 
-const linkStyles = {
-    fontFamily: 'Poppins-ExtraLight',
-    fontWeight: '600',
-    fontSize: '13px',
-    padding: '0px',
-    color: '#c9ccd8'
-}
-const sublistStyle = ({
-    pl: '30px',
+
+
+const sideBarTheme = createTheme({
+    components: {
+        MuiDrawer: {
+            styleOverrides: {
+                paper: {
+                    backgroundColor: "#0f141e"
+                }
+            },
+            variants: [
+                {
+                    props: {
+                        variant: "permanent",
+                        anchor: "left",
+                    },
+                }
+            ]
+        },
+        MuiListItemText: {
+            defaultProps: {
+                primaryTypographyProps: {
+                    fontFamily: 'Poppins-ExtraLight',
+                    fontWeight: '600',
+                    fontSize: '13px',
+                    padding: '0px',
+                    color: '#c9ccd8'
+                }
+            },
+        },
+        MuiListItemButton: {
+            defaultProps: {
+                style: {
+                    display: 'flex,',
+                    alignProperty: 'center',
+                    paddingBottom: '10px',
+                    width: '100%'
+                } 
+            },
+            styleOverrides: {
+                root: {
+                    '&:hover': {
+                        backgroundColor: 'grey'
+                    }
+                }
+            }
+        },
+        MuiListItemIcon: {
+            defaultProps: {
+                sx: {
+                    minWidth: '0px',
+                    minHeight: '0px'
+                }
+            }
+        }
+    },
 })
-
-
 
 function SidebarV2() {
     return (
             <Box >
-                <Drawer className="sidebar" variant="permanent" anchor='left' PaperProps={{
-                    sx: {
-                        backgroundColor: "#0f141e"
-                    }
-                }}>
+                <ThemeProvider theme={sideBarTheme}>
+                <Drawer className="sidebar" variant="permanent">
                     <List>
                         {sidebarProps.map((data, index) => (
                             <React.Fragment key={index}>
-                            <ListItem key={data.label}>
-                                <ListItemButton className="list-item-button" component={Link} to={data.link}>
-                                        <ListItemIcon sx={{minWidth: 0}}> <data.icon className="icon"/> </ListItemIcon>
-                                        <ListItemText primary={data.label}  primaryTypographyProps={linkStyles}/>
+                                <ListItemButton component={Link} to={data.link}>
+                                        <ListItemIcon> <data.icon className="icon"/> </ListItemIcon>
+                                        <ListItemText primary={data.label}/>
                                 </ListItemButton>
-                            </ListItem>
 
                             {data.sublist && (
                                 <Collapse in={true}>
-                                    <List disablePadding sx={sublistStyle}>
+                                    <List disablePadding>
                                         {data.sublist.map((subListItems) => (
-                                        <ListItem key={subListItems.label}>
-                                            <ListItemButton className="list-item-button" component={Link} to={subListItems.link}>
-                                                        <ListItemText primary={subListItems.label} primaryTypographyProps={linkStyles} />
+                                            <ListItemButton sx={{paddingLeft: 8}} component={Link} to={subListItems.link}>
+                                                        <ListItemText primary={subListItems.label}/>
                                             </ListItemButton>
-                                        </ListItem>
                                         ))}
                                     </List>
                                 </Collapse>
@@ -89,6 +127,7 @@ function SidebarV2() {
                         ))}
                     </List>
                 </Drawer>
+                </ThemeProvider>
             </Box>
     )
 }
