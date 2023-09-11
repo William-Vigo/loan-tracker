807d2c53f6fecf32bad1f6e45e5162aec38a0b8d
William-Vigo <d3rboyfresh123@hotmail.com>
2023-09-11 01:53:23 -0400

    removed button and just used icon

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -5,14 +5,12 @@ import {ReactComponent as SearchIcon} from "../../assets/search.svg"
 function SearchInput({ onChange}) {
   return (
     <Paper variant="outlined" style={{ display: 'flex', alignItems: 'center', padding: '2px', borderColor: "lightgray"}}>
-      <IconButton type="submit" aria-label="search" style={{ padding: '5px' }}>
-        <SearchIcon/>
-      </IconButton>
-      <InputBase
-        placeholder="Busca . . ."
-        style={{ marginLeft: '8px', flex: 1}}
-        onChange={onChange}
-      />
+        <SearchIcon type="submit" style={{ padding: '5px' }}/>
+        <InputBase
+            placeholder="Busca . . ."
+            style={{ marginLeft: '8px', flex: 1}}
+            onChange={onChange}
+        />
     </Paper>
   );
 }
